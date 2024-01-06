7864a3bdab31fa7ea90ce12974a994136539e899
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-06 05:51:32 -0600

    Finished Homework

--- HomeworkList/Program.cs
+++ HomeworkList/Program.cs
@@ -6,3 +6,20 @@
  * students to the Console.
  */
 
+string answer;
+//List<string> student =  new List<string>();
+string data = "Mike, Larry, Rob, Jone";
+List<string> student = data.Split(',').ToList();
+
+do
+{
+	//Add students to a class roster List
+	Console.WriteLine("Enter student's name to the class roster or else enter no: ");
+	answer = Console.ReadLine();
+	student.Add(answer);
+
+	
+} while ( answer.ToLower() != "no");
+
+// Print out the count of the students to the console
+Console.WriteLine($"{student.Count} students are enrolled to the class");
